9799e97e87d238c13995332606f725f1152faa28
Shrey Patel <shreypatel11@hotmail.com>
2022-06-09 17:43:53 -0400

    Made callback function and index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,30 @@
 const request = require('request');
-const breedName = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`
 
-request(url, function (error, response, body) {
-  // console.error('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // // console.log('body:', body); // Print the HTML for the Google homepage.
 
-  if (body === '[]') {
-    console.log('Cat not found');
-  } else {
+const fetchBreedDescription = (breedName, callback) => {
+  // This code will get information from the api using the requestion async function
+  // it needs to be parsed since it is all strings
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, function(error, response, body) {
     const data = JSON.parse(body);
-    console.log(data[0].description);
-  }
+
+    // When the function is called it will run the above code always.
 
 
+    // Once it reaches here if there is data at index 0 (from code run above),
+    // return null for the error (first paramater of the callback), and the description as desc (2nd para)
+    //   if there is no data found from the above code the error would not be null (instead will be 'No Cat Found')
+    //   and therefore truthy so the else statment is ran on index.js
+    if (data[0]) {
+      return callback(null, data[0].description);
+    } else {
+      return callback('No Cat Found.');
+    }
 
+  });
+};
 
-});
+module.exports = {fetchBreedDescription};
+
+// this is just making the function (declaring it)
+// when you actually call this function since there is a callback you would need to make the callback function in the 2nd parameter
